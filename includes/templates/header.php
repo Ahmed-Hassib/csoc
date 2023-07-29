cd16122eda9f41f0a070f0753eccf4b763692866
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-07-29 03:24:59 +0300

    create templates directory

---
+++ includes/templates/header.php
@@ -0,0 +1,17 @@
+<?php header('Accept-Encoding: deflate, gzip, compress, *');  // set accept encoding.?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- page title -->
+  <title><?php get_page_tilte($lang_file); ?></title>
+  <!-- css files -->
+  <?php include_once 'css-includes.php' ?>
+</head>
+<body dir="rtl">
+<?php 
+// set the default timezone to use.
+date_default_timezone_set('Africa/Cairo');
+?>

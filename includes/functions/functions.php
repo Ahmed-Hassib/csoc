e1e78ddd206058356bd94b87fcbccea55c2367b5
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-07 23:39:38 +0300

    add a new function to redirect where we want after the given seconds with displaying a specific message you are given

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -12,6 +12,36 @@ function get_page_tilte($file) {
   if (isset($page_title)) {
     echo lang(strtoupper($page_title), $file);
   } else {
-    echo lang('NOT ASSIGNED');
+    echo lang('NOT ASSIGNED', 'global_');
   }
+}
+
+/**
+ * redirect_home function v2
+ * This function accepts parameters
+ * $msg => echo the error message
+ * $seconds => seconds before redirect
+ */
+function redirect_home($msg = null, $url = null, $seconds = 3)
+{
+  // check the url
+  if ($url == null) {
+    $target_url = '../dashboard/index.php';
+  } else {
+    if ($url == 'back') {
+      $target_url = isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '../dashboard/index.php';
+    } else {
+      $target_url = $url;
+    }
+  }
+  // redirect page
+  header("refresh:$seconds;url=$target_url");
+  // check if empty message
+  if (!empty($msg) && $msg != null) {
+    echo $msg;
+  }
+  // show redirect messgae
+  echo "<div>" . lang('REDIRECT AUTO', 'global_') . " $seconds " . lang('SECOND', 'global_') . "</div>";
+  // exit
+  exit();
 }

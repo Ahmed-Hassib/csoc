767e3aaeda51d7ba1f2c34ef7b544a7a23c3c26d
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-07 23:41:26 +0300

    create header, navbar and footer elements and include it

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,6 +1,8 @@
-<?php header('Accept-Encoding: deflate, gzip, compress, *');  // set accept encoding.?>
+<?php header('Accept-Encoding: deflate, gzip, compress, *');  // set accept encoding.
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,8 +12,52 @@
   <!-- css files -->
   <?php include_once 'css-includes.php' ?>
 </head>
+
 <body dir="rtl">
-<?php 
-// set the default timezone to use.
-date_default_timezone_set('Africa/Cairo');
-?>
+  <?php
+  // set the default timezone to use.
+  date_default_timezone_set('Africa/Cairo');
+  // check if page have a preloader variable or not
+  if (isset($preloader) && $preloader == true) {
+    include_once 'preloader.php';
+  }
+  ?>
+
+
+  <?php if (isset($_SESSION['flash_message']) && isset($_SESSION['flash_message_icon']) && isset($_SESSION['flash_message_class']) && isset($_SESSION['flash_message_status'])) { ?>
+  <div class="m-0 container">
+    <div class="alert-flash-container">
+      <?php if (is_array($_SESSION['flash_message'])) { ?>
+      <?php foreach ($_SESSION['flash_message'] as $key => $message) { ?>
+      <div class="alert alert-<?php echo $_SESSION['flash_message_class'][$key]; ?> alert-flash-status" dir="rtl">
+        <i class="bi <?php echo $_SESSION['flash_message_icon'][$key] ?>"></i>
+        <?php echo lang($message, $_SESSION['flash_message_lang_file'][$key]) ?>
+        <button type="button" class="btn-close btn-close-left" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php } ?>
+      <?php } else { ?>
+      <div class="alert alert-<?php echo $_SESSION['flash_message_class']; ?> alert-flash-status" dir="rtl">
+        <i class="bi <?php echo $_SESSION['flash_message_icon'] ?>"></i>
+        <?php echo lang($_SESSION['flash_message'], $_SESSION['flash_message_lang_file']) ?>
+        <button type="button" class="btn-close btn-close-left" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php } ?>
+    </div>
+  </div>
+
+  <script>
+  let flash_alert = document.querySelectorAll('.alert-flash-status');
+
+  setTimeout(() => {
+    flash_alert.forEach(alert => {
+      alert.remove();
+    });
+  }, 10000);
+  </script>
+
+  <?php unset($_SESSION['flash_message']) ?>
+  <?php unset($_SESSION['flash_message_icon']) ?>
+  <?php unset($_SESSION['flash_message_class']) ?>
+  <?php unset($_SESSION['flash_message_status']) ?>
+  <?php unset($_SESSION['flash_message_lang_file']) ?>
+  <?php } ?>
---
+++ includes/templates/navbar.php
@@ -0,0 +1,65 @@
+<!-- start sidebar menu -->
+<div class="sidebar-menu sidebar-menu-right close">
+  <!-- start sidebar menu brand -->
+  <div class="sidebar-menu-brand" href="dashboard.php" <?php if (!isset($_SESSION['user_id'])) {
+                                                          echo "style='margin: auto'";
+                                                        } ?>>
+    <img src="<?php echo $assets . "air-defence-logo-2.png" ?>" class="sidebar-menu-logo-img"
+      alt="<?php echo isset($_SESSION['company_name']) ? $_SESSION['company_name'] : lang('NOT ASSIGNED') ?>"
+      id="company-img-brand">
+    <!-- <img  src="<?php echo $assets ?>jsl.jpeg" > -->
+    <span class="sidebar-menu-logo-name text-uppercase ">csoc 830</span>
+    <!-- close icon displayed in small screens -->
+    <span class="close-btn"><i class="bi bi-x"></i></span>
+  </div>
+  <!-- end sidebar menu brand -->
+  <!-- start sidebar menu content -->
+  <ul class="nav-links">
+    <!-- start dashboard page link -->
+    <li>
+      <a href="<?php echo $nav_up_level ?>dashboard/index.php">
+        <i class="bi bi-grid"></i>
+        <span class="link-name"><?php echo lang('DASHBOARD') ?></span>
+      </a>
+      <!-- start blank sub menu -->
+      <ul class="sub-menu blank">
+        <li>
+          <a href="<?php echo $nav_up_level ?>dashboard/index.php">
+            <span class="link-name"><?php echo lang('DASHBOARD') ?></span>
+          </a>
+        </li>
+      </ul>
+      <!-- end blank sub menu -->
+    </li>
+    <!-- end dashboard page link -->
+  </ul>
+  <!-- end sidebar menu content -->
+</div>
+<!-- end sidebar menu -->
+
+<div class="top-navbar top-navbar-right">
+  <div class="top-navbar-content">
+    <i class="bi bi-list sidebar-menubtn"></i>
+    <a href="" class="btn btn-outline-light py-1 fs-12 mx-3">
+      <span><?php echo lang('REFRESH SESSION') ?></span>
+    </a>
+
+    <ul class="nav-links me-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <?php echo $_SESSION['username'] ?>
+        </a>
+        <ul class="dropdown-menu-end dropdown-menu">
+          <li>
+            <a class="dropdown-item" href="<?php echo $src ?>logout.php">
+              <i class="bi bi-box-arrow-right"></i>
+              logout
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div class="main-content">

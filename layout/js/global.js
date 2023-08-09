73438a539b10809f348825cfdd4c36b3c12f856f
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-09 13:38:41 +0300

    create a function accepts a parameter to check it given param is required field if form to add an astrisk ti it

--- layout/js/global.js
+++ layout/js/global.js
@@ -1,4 +1,43 @@
+var inputs = document.getElementsByTagName("input");
+var selects = document.getElementsByTagName("select");
 
+(function () {
+  // add astrisk
+  add_astrisk(selects);
+  add_astrisk(inputs);
+})()
+
+
+/**
+ * add_astrisk function
+ * this function is used to add astrisk mark on required inputs
+ */
+function add_astrisk(inputs) {
+  // loop on inputs
+  for (const input of inputs) {
+    // add astrisk on required field
+    if (input.hasAttribute("required") && !input.hasAttribute("data-no-astrisk")) {
+      // create span
+      let astrisk = document.createElement("span");
+      // add some classes
+      astrisk.classList.add("text-danger", "astrisk");
+      // check system language
+      if (localStorage['systemLang'] == 'ar') {
+        // add some classes
+        astrisk.classList.add("astrisk-left");
+      } else if (localStorage['systemLang'] == 'en') {
+        // add some classes
+        astrisk.classList.add("astrisk-right");
+      } else {
+        // add some classes
+        astrisk.classList.add("astrisk-left");
+      }
+      astrisk.textContent = "*";
+      // append astrisk
+      input.parentElement.appendChild(astrisk);
+    }
+  }
+}
 
 /**
  * show_pass function
@@ -13,3 +52,4 @@ function show_pass(btn) {
     btn.previousElementSibling.setAttribute("type", "password");
   }
 }
+

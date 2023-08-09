672a8e0f7110ab9fc1ccba7d82b18cacae3ce8f3
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-10 02:37:06 +0300

    create units page language and css file

--- includes/languages/units.php
+++ includes/languages/units.php
@@ -6,6 +6,7 @@
 function units($phrase)
 {
   static $lang = array(
+    // units words
     'UNITS'               => 'الوحدات',
     'UNITS DASHBOARD'     => 'الصفحة الرئيسية للوحدات',
     'UNIT NAME'           => 'اسم الوحدة',
@@ -15,6 +16,18 @@ function units($phrase)
     'UNIT LEADER NAME'    => 'اسم قائد الوحدة',
     'UNIT LEADER PHONE'   => 'رقم تليفون قائد الوحدة',
     'UNIT ADDRESS'        => 'عنوان الوحدة',
+    'EDIT UNIT'           => 'تعديل بيانات وحدة',
+
+    // units messages
+    'UNIT NAME IS REQUIRED'     => 'اسم الوحدة مطلوب',
+    'UNIT TYPE IS REQUIRED'     => 'نوع الوحدة مطلوب',
+    'LEADER RANK IS REQUIRED'   => 'رتبة قائد الوحدة مطلوبة',
+    'LEADER NAME IS REQUIRED'   => 'اسم قائد الوحدة مطلوب',
+
+    'INSERTED'        => 'تم اضافة بيانات الوحدة بنجاح',
+    'UPDATED'         => 'تم تعديل بيانات الوحدة بنجاح',
+    'DELETED'         => 'تم حذف بيانات الوحدة بنجاح',
+
   );
   // return the phrase
   return $lang[$phrase];

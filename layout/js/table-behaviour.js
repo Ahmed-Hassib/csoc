a096ec0bf2e50d74c4c3be21490bdd7b192bec12
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-10 23:36:13 +0300

    create tables style and behaviour

---
+++ layout/js/table-behaviour.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  // get table columns
+  let dataTable = $('table.display');
+
+  if (dataTable != null) {
+    // get the table
+    var table = $('table.display').DataTable({
+      "scrollX": true,
+      ordering: true,
+      stateSave: true,
+      stateDuration: -1,
+      lengthMenu: [
+        [10, 25, 50, 100, -1],
+        [10, 25, 50, 100, 'All'],
+      ],
+      dom: '<"#datatables-buttons.w-auto mb-2"B><".row g-3"<".col-sm-12 col-lg-6"f><".col-sm-12 col-lg-6 text-start"l>>ti<".pagination-right"p>',
+      buttons: [
+        { extend: 'excel', className: 'btn btn-outline-primary fs-12 py-1', text: 'تحميل الجدول' },
+        { extend: 'colvis', className: 'btn btn-outline-primary fs-12 py-1', columns: ':not(.noVis)', text: 'رؤية الأعمدة' },
+      ],
+      columnDefs: [{ targets: [0, -1], className: 'noVis' }],
+
+      "language": {
+        "emptyTable": "لا توجد بيانات للعرض",
+        "info": "عرض _START_ الي _END_ من _TOTAL_ صف",
+        "infoEmpty": "عرض 0 الي 0 من 0 صف",
+        "infoFiltered": "(تم البحث من خلال _MAX_ صف)",
+        "lengthMenu": "عرض _MENU_ صفوف",
+        "loadingRecords": "جارى تحميل البيانات...",
+        "search": "البحث",
+        "zeroRecords": "لا توجد بيانات متطابقة للبحث المطلوب",
+        "paginate": {
+          "first": "الاول",
+          "last": "الاخير",
+          "next": "التالى",
+          "previous": "السابق"
+        },
+      }
+    });
+  }
+
+  // select all data tables controls
+  let dataTable_control_btn = $('.dt-buttons button');
+  // loop on it
+  dataTable_control_btn.each(function () {
+    if (dataTable_control_btn.hasClass('btn-secondary')) {
+      dataTable_control_btn.removeClass('btn-secondary')
+    }
+  })
+});

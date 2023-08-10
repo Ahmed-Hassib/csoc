3552fe8daecf8da2b32da8b43dede612314550ca
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-10 23:34:47 +0300

    update parameter function of get page title

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -6,14 +6,14 @@
  * Contain global variable can be access from anywhere
  * get title page from the page and display it
  */
-function get_page_tilte($file)
+function get_page_tilte($file = 'global_')
 {
   global $page_title; // page title
   // check if set or not
   if (isset($page_title)) {
     echo lang(strtoupper($page_title), $file);
   } else {
-    echo lang('NOT ASSIGNED', 'global_');
+    echo lang('NOT ASSIGNED', $file);
   }
 }
 

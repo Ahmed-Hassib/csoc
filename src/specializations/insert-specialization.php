3394b1e513f15d2a04bba36b151036fe04454aa8
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-08-20 00:03:38 +0300

    update algorithm to check the given array if empty or not

--- src/specializations/insert-specialization.php
+++ src/specializations/insert-specialization.php
@@ -9,32 +9,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // create an object of Soldier class
   $spec_obj = new Specialization();
 
-  // flag
-  $flag = 0;
+  // check array counter
+  if (count($spec_name) > 0) {
+    // flag
+    $flag = 0;
 
-  foreach ($spec_name as $key => $spec) {
-    if (!empty($spec)) {
-      // insert new specialization
-      $is_inserted = $spec_obj->insert_new_specialization(array($spec, $spec_note[$key]));
-      // increase flag
-      if ($is_inserted) $flag += 1;
+    foreach ($spec_name as $key => $spec) {
+      if (!empty($spec)) {
+        // insert new specialization
+        $is_inserted = $spec_obj->insert_new_specialization(array($spec, $spec_note[$key]));
+        // increase flag
+        if ($is_inserted) $flag += 1;
+      }
     }
-  }
 
-  if ($flag == 0) {
-    // prepare flash session variables
-    $_SESSION['flash_message'] = 'QUERY PROBLEM';
-    $_SESSION['flash_message_icon'] = 'bi-exclamation-triangle-fill';
-    $_SESSION['flash_message_class'] = 'danger';
-    $_SESSION['flash_message_status'] = false;
-    $_SESSION['flash_message_lang_file'] = 'global_';
-  } else {
-    // prepare flash session variables
-    $_SESSION['flash_message'] = $flag > 1 ? 'INSERTED ALL' : 'INSERTED';
-    $_SESSION['flash_message_icon'] = 'bi-check2-circle-fill';
-    $_SESSION['flash_message_class'] = 'success';
-    $_SESSION['flash_message_status'] = true;
-    $_SESSION['flash_message_lang_file'] = 'specializations';
+    if ($flag == 0) {
+      // prepare flash session variables
+      $_SESSION['flash_message'] = 'QUERY PROBLEM';
+      $_SESSION['flash_message_icon'] = 'bi-exclamation-triangle-fill';
+      $_SESSION['flash_message_class'] = 'danger';
+      $_SESSION['flash_message_status'] = false;
+      $_SESSION['flash_message_lang_file'] = 'global_';
+    } else {
+      // prepare flash session variables
+      $_SESSION['flash_message'] = $flag > 1 ? 'INSERTED ALL' : 'INSERTED';
+      $_SESSION['flash_message_icon'] = 'bi-check2-circle-fill';
+      $_SESSION['flash_message_class'] = 'success';
+      $_SESSION['flash_message_status'] = true;
+      $_SESSION['flash_message_lang_file'] = 'specializations';
+    }
   }
 } else {
   // prepare flash session variables
